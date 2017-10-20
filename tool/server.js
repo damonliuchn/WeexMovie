26856783f423477bf5746b07a054f85a410192ba
liumeng <liumeng>
2017-10-20 18:45:05 +0800

    使用express 做 静态服务器

---
+++ tool/server.js
@@ -0,0 +1,12 @@
+﻿var express = require('express');
+var app = express();
+var router = express.Router();
+var fs = require("fs");
+var path = require("path");
+
+app.use('/dist',express.static('dist'));
+app.use('/node_modules',express.static('node_modules'));
+// app.use('/', router);
+var port = 10004;
+app.listen(port);
+console.log('Magic happens on port ' + port);
