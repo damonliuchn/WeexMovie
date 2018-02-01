d630cb17c4f9f573e0217c710037da0bbeb7224d
MasonLiuChn <MasonLiuChn@gmail.com>
2018-02-01 12:43:47 +0800

    vue router to  native

--- src/entry-web.js
+++ src/entry-web.js
@@ -3,15 +3,20 @@
  * native 环境下自动导入 weex对象 和  vue对象
  */
 import "weex-vue-render"
+
 /**
  * 导入vue-router，仅web环境可用
  */
 import VueRouter from 'vue-router'
 import routes from './router-web'
 Vue.use(VueRouter);
-
 const router = new VueRouter({
     //mode: 'hash',
     routes
 });
+
+/**
+ * 创建vue实例 挂载到html的dom上， 在native环境下，这一步是weex sdk 和 native代码做的 创建Vue实例 挂载到Native activity上
+ * html 和 native activity相类似  是一个容器
+ */
 new Vue({el: '#root', router});
